7b13869758e0d4cf6eb470ddeb9d3f172375e25f
John Swinbank <swinbank@lsst.org>
2020-07-23 14:49:11 -0700

    Nicer listing of predecessors

--- milestones/predecessors.py
+++ milestones/predecessors.py
@@ -3,7 +3,11 @@ __all__ = ["predecessors"]
 
 def predecessors(args, mc):
     for ms in sorted(mc.filter("LDM-503"), key=lambda x: (x.due, x.code)):
+        print(f"{ms.code} ({ms.name}) :")
         predecessors = [prems for prems in mc.filter("DM-")
                         if prems.code in ms.predecessors]
+        if not predecessors:
+            print(f"    (No prdecessors)")
+
         for prems in sorted(predecessors, key=lambda x: (x.due, x.code)):
-            print(ms.code + " : " + prems.code)
+            print(f"    {prems.code} ({prems.name})")
