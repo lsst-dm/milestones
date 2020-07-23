e031a91c2bc68cd963f7db8dfeacef30f3581ae0
John Swinbank <swinbank@lsst.org>
2020-07-23 14:48:38 -0700

    Clean up / correct burndown chart.

--- milestones/burndown.py
+++ milestones/burndown.py
@@ -34,46 +34,19 @@ def burndown(args, mc):
                 model_remain -= 1
             if ms.completed and ms.completed <= date:
                 actual_remain -= 1
+
         model.append(model_remain)
-        try:
-            # We'll guess that the last month for which we have data is the
-            # last month in whch at least one milestone was completed. This
-            # isn't necessarily strictly correct, but it's not bad.
-            if actual_remain < actual[-1]:
-                last_achieved_month = date
-        except IndexError:
-            pass
         actual.append(actual_remain)
 
-    plt.plot(month_starts, model, label="Baseline May 2019 ")
+    last_achieved_month = None
+    for ms in milestones:
+        if ms.completed and (not last_achieved_month or last_achieved_month < ms.completed):
+            last_achieved_month = ms.completed
 
-    # Dumped from running the code with old PMCS data.
-    # Plotted for comparison purposes.
-    bl2018 = [201, 196, 196, 196, 194, 194, 194, 186, 186, 185, 177, 175, 175,
-              143, 140, 139, 139, 127, 127, 126, 115, 110, 109, 104, 98, 72,
-              72, 68, 68, 66, 66, 63, 63, 61, 59, 51, 44, 42, 36, 34, 31, 31,
-              31, 31, 31, 30, 27, 27, 22, 14, 14, 14, 14, 14, 14, 13, 12, 11,
-              11, 9, 7, 4, 4, 4, 2, 2, 2, 2]
-
-    plt.plot(month_starts, bl2018, label="Baseline June 2018")
-
+    plt.plot(month_starts, model, label="Baseline")
 
     achieved_months = pd.date_range(start_date, last_achieved_month, freq='M')
     plt.plot(achieved_months, actual[:len(achieved_months)], label="Achieved")
-
-    # Removed glide slope to make the plt less noisy.
-#    glide_slope = [len(milestones) - i * (len(milestones) / len(month_starts)) for i in range(len(month_starts))]
-#    plt.plot(month_starts, glide_slope, label="Glide slope")
-
-#    obsolete_ms = [
-#        "DLP-538", "DLP-541", "DLP-808", "DLP-799", "DLP-458", "DM-NCSA-5", "DM-NCSA-7"
-#    ]
-#
-##    flat = [actual[len(achieved_months)] - len(obsolete_ms) for i in range(len(month_starts))]
-
-    # Obsolete illustration, removed.
-#    timescale = month_starts[len(achieved_months)-3:len(achieved_months)+2]
-#    plt.plot(timescale, flat[:5], label="Corrected")
 
     plt.xlabel("Date")
     plt.ylabel("Open Milestones")
