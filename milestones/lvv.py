d5e3c437ef844e434f243dd5c6a0c616692ee678
Leanne Guy <leanne.guy@lsst.org>
2021-02-15 23:57:03 -0700

    Update requirement extraction

--- milestones/lvv.py
+++ milestones/lvv.py
@@ -1,6 +1,8 @@
 import requests
 from marshmallow import Schema, fields, pre_load, EXCLUDE
 
+__all__ = ["extract_ve_details"]
+
 JIRA_INSTANCE = "https://jira.lsstcorp.org"
 ISSUE_UI_URL = f"{JIRA_INSTANCE}/browse/{{issue}}"
 JIRA_API = f"{JIRA_INSTANCE}/rest/api/latest"
@@ -21,22 +23,38 @@ class VerificationE(Schema):
     summary = fields.String()
     req_id = fields.String()
     req_priority = fields.String()
-    req_milestone = fields.String()
+    # req_milestone = fields.String(allow_none=True)
     jira_url = fields.String()
 
-    @pre_load(pass_many=True)
+    @pre_load(pass_many=False)
     def extract_fields(self, data, **kwargs):
         """Extract the needed fields"""
 
         out_data = dict()
-        out_data["key"] = str(data["key"]).strip()
+        out_data["key"] = str(data["key"])
         data_fields = data["fields"]
         out_data["summary"] = data_fields["summary"]
         out_data["jira_url"] = ISSUE_UI_URL.format(issue=data["key"])
-        out_data["req_milestone"] = data_fields["labels"][0]
 
-        out_data["req_priority"] = "None"
+        # out_data['req_milestone'] = None
+        # if data_fields["labels"] is not None and len(data_fields["labels"]) > 0:
+        #     out_data["req_milestone"] = data_fields["labels"][0]
+
+        out_data["req_priority"] = None
         if data_fields["priority"]:
             out_data["req_priority"] = data_fields["priority"]["name"]
 
         return out_data
+
+
+def extract_ve_details(lvv_key):
+    """
+    Extract the details from Jira for an LVV
+
+    :return: Object with VE details
+    """
+    rs = requests.Session()
+    jve_res = rs.get(ISSUE_URL.format(issue=lvv_key)).json()
+    ve_details = VerificationE(unknown=EXCLUDE).load(jve_res)
+    rs.close()
+    return ve_details
