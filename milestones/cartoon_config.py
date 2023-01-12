0b860f6cca5fb5f994e706bd1fa87fbb96cb9854
Robert Lupton the Good <rhl@astro.princeton.edu>
2023-01-11 19:24:48 -0500

    Adjust legend position to avoid camera

--- milestones/cartoon_config.py
+++ milestones/cartoon_config.py
@@ -10,7 +10,7 @@ __all__ = ["categoryNrow", "categoryGrouping", "categoryColors",
 nRowOfMilestones = 3                    # number of rows of milestones
 milestoneHeight = 4                     # height of each row of milestones
 milestoneWidth = 20                     # width of labels; used to wrap text
-legend_location = (0.81, 0.02)          # plt.legend(loc=legend_location)
+legend_location = (0.83, 0.02)          # plt.legend(loc=legend_location)
 today_height = 0.5                      # height of today's label; fraction of y-axis
 
 #
